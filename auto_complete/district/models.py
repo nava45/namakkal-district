22f6c08c16969a764307783b81b7d0a8ffea5047
nava45 <nava.nmr@gmail.com>
2015-06-08 18:58:32 +0530

    Inital commit with db models

---
+++ auto_complete/district/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True, db_index=True)
+
+    def __unicode__(self):
+        return "<%s>" %self.name
+
+class Location(models.Model):
+    place = models.CharField(max_length=100, db_index=True, unique=True)
+    latitude = models.CharField(max_length=100, blank=True, null=True)
+    longitude = models.CharField(max_length=100, blank=True, null=True)
+
+    def __unicode__(self):
+        return "<%s>" %self.place
+
+class Item(models.Model):
+    name = models.CharField(max_length=300)
+    address = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    contact_details = models.TextField(blank=True, null=True)
+    special_info = models.TextField(blank=True, null=True)
+
+    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location)
+
+    def __unicode__(self):
+        return "<%s-%s-%s>" %(self.name, self.category.name, \
+                              self.location.place)
